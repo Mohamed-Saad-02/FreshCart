4695e9be7d713142998c36bb6be4c002718b971f
Mohamed-Saad-02 <mohamedsaad63c@gmail.com>
2024-08-31 11:33:47 +0300

    update order

--- src/services/order.js
+++ src/services/order.js
@@ -3,7 +3,7 @@ import { BASE_URL, userToken } from "../utils/constants";
 
 export async function createCashOrder(cartId, shippingAddress) {
   const { data } = await axios.post(
-    `${BASE_URL}/api/v1/orders/checkout-session/${cartId}?url=http://localhost:5173`,
+    `${BASE_URL}/api/v1/orders/checkout-session/${cartId}?url=https://freshcart-0.netlify.app`,
     { shippingAddress },
     { headers: { token: userToken } }
   );

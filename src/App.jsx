9f6ae6a6c4bf0e086ba911d467edd262639fed0a
Mohamed-Saad-02 <mohamedsaad63c@gmail.com>
2024-09-05 02:37:26 +0300

    update footer and seo

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ const router = createBrowserRouter([
     path: "/",
     element: (
       <ProtectedAuth>
-        <AuthLayout />,
+        <AuthLayout />
       </ProtectedAuth>
     ),
     children: [
